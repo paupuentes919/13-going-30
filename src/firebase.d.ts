5c01e5a6270d50c7d11ecd98209557d121899781
paupuentes919 <paupuentes919@gmail.com>
2024-03-28 15:49:51 -0300

    change firebase.js to .ts

--- src/firebase.d.ts
+++ src/firebase.d.ts
@@ -1,23 +1,23 @@
-// Definición de tipos para el objeto de configuración de Firebase
-interface FirebaseConfig {
-  apiKey: string
-  authDomain: string
-  databaseURL: string
-  projectId: string
-  storageBucket: string
-  messagingSenderId: string
-  appId: string
-  measurementId?: string
-}
+// // Definición de tipos para el objeto de configuración de Firebase
+// interface FirebaseConfig {
+//   apiKey: string
+//   authDomain: string
+//   databaseURL: string
+//   projectId: string
+//   storageBucket: string
+//   messagingSenderId: string
+//   appId: string
+//   measurementId?: string
+// }
 
-// Definición de tipos para el objeto de inicialización de Firebase
-interface FirebaseApp {
-  analytics?: any // Puedes definir tipos más específicos si los necesitas
-  firestore(): FirebaseFirestore.Firestore
-  // Otros métodos y propiedades de Firebase que estés utilizando en tu proyecto
-}
+// // Definición de tipos para el objeto de inicialización de Firebase
+// interface FirebaseApp {
+//   analytics?: any // Puedes definir tipos más específicos si los necesitas
+//   firestore(): FirebaseFirestore.Firestore
+//   // Otros métodos y propiedades de Firebase que estés utilizando en tu proyecto
+// }
 
-// Objeto global de Firebase
-declare const firebase: FirebaseApp
+// // Objeto global de Firebase
+// declare const firebase: FirebaseApp
 
-export { FirebaseConfig, FirebaseApp, firebase }
+// export { FirebaseConfig, FirebaseApp, firebase }
